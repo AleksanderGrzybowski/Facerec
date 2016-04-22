cfa3ddb0b8e8045734ecd9472e8cabd69def2ae9
Aleksander Grzybowski <kelog@orange.pl>
2016-04-22 13:04:53 +0200

    Improve frontend with my questionable skillz

--- web/public/app.js
+++ web/public/app.js
@@ -1,21 +1,36 @@
+/* Yes, this $ soup should be rewritten in Angular/React. But it works */
+
 Webcam.attach('#camera');
 
-$('#trigger').click(function triggered() {
-    Webcam.snap(function (dataUri) {
-        var data = extractData(dataUri);
+var $trigger = $('#trigger');
+var $result = $('#result-target');
 
+$trigger.click(function triggered() {
+
+    $trigger.addClass('disabled').text('Please wait...');
+    $result.removeClass('alert-success').removeClass('alert-danger').text('');
+
+    Webcam.snap(function (dataUri) {
         $.ajax({
             type: 'POST',
             url: '/recognize',
-            data: JSON.stringify({data: data}),
-            success: function (response) {
-                console.log(response)
-            },
+            data: JSON.stringify({data: extractData(dataUri)}),
+            success: successHandler,
             contentType: 'application/json',
             dataType: 'json'
         });
     });
 });
+
+function successHandler(response) {
+    if (response.success) {
+        $result.text('You are person of id = ' + response.prediction).addClass('alert-success');
+    } else {
+        $result.text('Recognition failed, please try again').addClass('alert-danger');
+    }
+
+    $trigger.removeClass('disabled').text('Capture!');
+}
 
 function extractData(dataUri) {
     return dataUri.substring(23);
